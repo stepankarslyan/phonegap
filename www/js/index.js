bb2c12675ad2d2bcff843cdc546cd1a0e211a103
Stepan Gharslyan <stepan.gharslyan@gmail.com>
2013-11-16 18:17:38 +0400

    first files

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+
+var online = navigator.onLine;
+if (online === true) {
+	alert ("online");
+	//window.location.assign("http://www.google.com");
+}
+else {
+
+alert("Hello you are offline please connect to internet !");
+}
+
