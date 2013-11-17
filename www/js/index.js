9bbcc5c3df7e325dc2b4138c15043ef19d4977c8
Levon <Levon.hakopyan@mail.ru>
2013-11-17 17:36:05 +0400

    finishing first step

--- www/js/index.js
+++ www/js/index.js
@@ -19,7 +19,7 @@ function onDeviceReady() {
 	var networkState = checkConnection();
 	/* load local files if there is not network connection */
     if (networkState == Connection.NONE) {
-	    window.location="www/index.html";
+	    window.location="www/tamplate/offline.html";
     }
     else {
 			 window.location="http://192.168.1.4:8080";
