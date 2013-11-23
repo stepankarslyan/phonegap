6fcd117ac597df456e245dba44af423996354869
Stepan Gharslyan <stepan.gharslyan@gmail.com>
2013-11-23 10:40:57 +0400

    refreshing the files

--- www/js/script.js
+++ www/js/script.js
@@ -2,14 +2,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function checkConnection() {
    var networkState = navigator.network.connection.type;
-   var states = {};
-   states[Connection.UNKNOWN]  = 'Unknown connection';
-   states[Connection.ETHERNET] = 'Ethernet connection';
-   states[Connection.WIFI]     = 'WiFi connection';
-   states[Connection.CELL_2G]  = 'Cell 2G connection';
-   states[Connection.CELL_3G]  = 'Cell 3G connection';
-   states[Connection.CELL_4G]  = 'Cell 4G connection';
-   states[Connection.NONE]     = 'No network connection';
   
    return networkState;
   
@@ -19,9 +11,9 @@ function onDeviceReady() {
 	var networkState = checkConnection();
 	/* load local files if there is not network connection */
     if (networkState == Connection.NONE) {
-	    window.location="www/index.html"; 
+	    window.location="www/templates/offline.html"; 
     }
     else {
-			 window.location="http://192.168.1.4:8080";
+			 window.location="http://192.168.10.112:8080";
     }
 }
