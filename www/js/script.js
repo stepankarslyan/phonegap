fac580ef1a861d6f602f030cd54c78e67a60745b
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-11-23 17:35:24 +0100

    Update script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,17 +1,24 @@
+/**
+ * Why do you put a different name for file and for object?
+ * app or script is giving no information on what this code do, choose more descriptive name, that answer what does this object do?
+ **/
 var app = {
 	
 	openLocation: function(location) {
 		window.location = location;
 	},
 	
+	// Always prefer positive logic -> isOnline is better than isOffline
 	isOffline: function () {
+		// Can't you find more simple syntax? like
+		// return navigator.network.connection.type != Connection.NONE 
 		var status;
 		var networkState = navigator.network.connection.type;
 		
 		if (networkState == Connection.NONE) {
 			status = true;
 		}
-		else { status = false;
+		else { status = false; // Don't put code after { but on next line
 		}
 		
 		return status;
@@ -20,10 +27,10 @@ var app = {
 	onDeviceReady: function() {
 		// load local files if there is not network connection 
 		if (app.isOffline()) {
-		  app.openLocation("www/offline/index.html");
+		  app.openLocation("www/offline/index.html"); // Probem with lines or tabs
 		}
 		else {
-			app.openLocation("http://192.168.10.112:8080");
+			app.openLocation("http://192.168.10.112:8080"); // Put this string in a property of object better than in code
 		}
 	},
 };
