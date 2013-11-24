e4ea087ec63a627e5e6b4286042f2ccdf672b998
Levon <Levon.hakopyan@mail.ru>
2013-11-24 17:33:26 +0400

    correcting and updating files

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,44 @@
 var app = {
+	
+	showMyLocation: function() {
+	
+		var onSuccess = function(position) {
+			var element = document.getElementById('geolocation');
+			
+			element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+				'Longitude: ' + position.coords.longitude + '<br />' +
+				'Altitude: ' + position.coords.altitude + '<br />' +
+				'Accuracy: ' + position.coords.accuracy + '<br />' +
+				'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+				'Heading: ' + position.coords.heading + '<br />' +
+				'Speed: ' + position.coords.speed + '<br />' +
+				'Timestamp: ' + position.timestamp + '<br />';
+		};
 
+		// onError Callback receives a PositionError object
+		var onError = function(error) {
+				alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+		}
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	},
+	/*
 	onlineLocation: "http://192.168.10.112:8080",
 	offlineLocation: "www/offline/index.html",
 	openLocation: function(location) {
 		window.location = location;
 	},
-	
+	*/
 	isOnline: function () {
 		return navigator.network.connection.type != Connection.NONE;
 	},
 
 	onDeviceReady: function() {
 		// load local files if there is not network connection 
-		// i will use only local page to test geolocation plugin
 		if (app.isOnline()) {
-		  app.openLocation(app.offlineLocation);
-	/*	}
-		else {
+		  app.showMyLocation();
+		  //app.openLocation(app.onlineLocation);
+		}
+	/*	else {
 			app.openLocation(app.offlineLocation);
 		}
 	*/
