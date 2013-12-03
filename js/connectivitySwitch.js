589b578330853d07bd767abff92f64db683232cf
Levon <Levon.hakopyan@mail.ru>
2013-12-04 01:14:12 +0400

    update for review

---
+++ js/connectivitySwitch.js
@@ -0,0 +1,27 @@
+var connectivitySwitch = {	
+
+	onlineLocation: "http://192.168.10.112:8080",
+	offlineLocation: "www/offline/index.html",
+
+	onDeviceReady: function() {
+	
+		if (connectivitySwitch.isOnline()) {
+			connectivitySwitch.openLocation(connectivitySwitch.onlineLocation);
+		}
+		else {
+			connectivitySwitch.openLocation(connectivitySwitch.offlineLocation);
+		}
+	},
+
+	isOnline: function() {
+		return true //navigator.network.connection.type != Connection.NONE;
+	},
+	
+	openLocation: function(location) { 
+		window.location = location;
+	}
+
+};
+
+document.addEventListener("deviceready", connectivitySwitch.onDeviceReady, false);
+
