90cfa5fb2433ca0481a37b714b16d0af91733714
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-04 13:38:46 +0100

    Update connectivitySwitch.js

--- www/js/connectivitySwitch.js
+++ www/js/connectivitySwitch.js
@@ -1,8 +1,9 @@
+// Rename the object connectivitySwitch
 var app = {
 
 	onlineLocation: "http://192.168.10.112:8080",
 	offlineLocation: "www/offline/index.html",
-	openLocation: function(location) {
+	openLocation: function(location) { // Always add line before and after function
 		window.location = location;
 	},
 	
@@ -10,10 +11,12 @@ var app = {
 		return navigator.network.connection.type != Connection.NONE;
 	},
 
+	// Rename by what the function do not by when the function is called
+	// switch: function(config) {...};
 	onDeviceReady: function() {
 		
 		if (app.isOnline()) {
-		  app.openLocation(onlineLocation);
+		  app.openLocation(onlineLocation); // Use the same space to indent your code
 		}
 		else {
 			app.openLocation(app.offlineLocation);
@@ -22,5 +25,13 @@ var app = {
 	},
 };
 
+// Move this line of code in index.html with anonymous function
+// You can even put a config object that will make the switch object reusable
+// document.addEventListener("deviceready", function() {
+// 	connectivitySwitch.switch({
+//		onlineLocation: "http://192.168.10.112:8080",
+// 		offlineLocation: "www/offline/index.html"
+//	});
+// }, false);
 document.addEventListener("deviceready", app.onDeviceReady, false);
 
