e8e013030c2598f73f0cef9af0fbc526088c1459
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-07 09:50:46 +0100

    Update connectivitySwitch.js
    Code review
    - Be aware of function scope
    - Name method with what they do not when they are called
    - Put not reusable code in calling code
    - Use config object to give not reusable values

--- js/connectivitySwitch.js
+++ js/connectivitySwitch.js
@@ -1,18 +1,34 @@
 var connectivitySwitch = {	
 
+	// Move this in a config param of switch function
 	onlineLocation: "http://192.168.10.112:8080",
 	offlineLocation: "www/offline/index.html",
-
+	
+	
+	// Change this method for 
+	/*
+	switch: function(config) {
+		// using config.onlineLocation and config.offlineLocation	
+	},
+	*/
+	
 	onDeviceReady: function() {
 		if (connectivitySwitch.isOnline()) {
+			// use this.openLocation, if it's not workign it's because you call the function without scope
+			// to avoid this don't pass onDeviceReady as parameter but an anonymous function
+			// calling connectivitySwitch.onDeviceReady
 			connectivitySwitch.openLocation(connectivitySwitch.onlineLocation);
 		}
 		else {
+			// Idem
 			connectivitySwitch.openLocation(connectivitySwitch.offlineLocation);
 		}
 	},
 
 	isOnline: function() {
+		// Don't forget debug code when you push
+		// One method is to add TODO in your comments when you have something not finished or 
+		// not to be pushed and search for this TODO before pushing
 		return true //navigator.network.connection.type != Connection.NONE;
 	},
 	
@@ -22,5 +38,7 @@ var connectivitySwitch = {
 
 };
 
+// Move this in index.html
+// Use anonymous function to avoid method call without scope
 document.addEventListener("deviceready", connectivitySwitch.onDeviceReady, false);
 
