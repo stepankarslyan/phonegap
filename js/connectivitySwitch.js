6d23ae3e0e0a002c72c56e41f68f421fe0a7e3d8
Levon <lev_hak@mail.ru>
2013-12-28 14:50:12 +0300

    Update connectivitySwitch.js

--- js/connectivitySwitch.js
+++ js/connectivitySwitch.js
@@ -10,7 +10,7 @@ var connectivitySwitch = {
 	},
 
 	isOnline: function() {
-		return true;//navigator.network.connection.type != Connection.NONE;
+		return navigator.network.connection.type != Connection.NONE;
 	},
 	
 	openLocation: function(location) { 
