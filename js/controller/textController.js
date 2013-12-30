034209c87d7bb81ede8d5722a9bc9a9c47f27c68
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-30 18:47:59 +0100

    Update textController.js

--- js/controller/textController.js
+++ js/controller/textController.js
@@ -1,6 +1,9 @@
 angular.module('app').
 controller('textController', ['$scope', function($scope) {
 
+	// Use controller object
+	// textController is not a well choosen name, use something corresponding to business vocabulary not to technical vocabulary
+
 	$scope.text = {
 		location: 'Push buttom to send to server your location information',
 		camera: 'Push buttom to capture photo and send to server automaticly'
