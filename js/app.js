85a4b77e79dca5a7a3fd487c940655da58b01c3a
Olivier Nicollet <olivier.nicollet@gmail.com>
2013-12-30 18:52:23 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 angular.module('app', []).
 config(['$routeProvider', function ($routeProvider) { 
 	$routeProvider.
-		when('/online', { templateUrl: '/templates/online.html'}).
+		when('/online', { templateUrl: '/templates/online.html'}). // link controller to view here
 		when('/offline', { templateUrl: '/templates/offline.html'}).
 		otherwise({ redirectTo: '/' });
 }]).
+
+// Define controller in a file corresponding to the controller name in the controller folder not in app.js
 controller('appController', ['$scope', 'connectivitySwitchService', function($scope, connectivitySwitchService) {
 
 	$scope.connectivitySwitch = function() {
