b7b9e5f57a6531b623c0f2c232476e434e015642
Levon Hakopyan <levon.hakopyan@gmail.com>
2014-01-18 02:32:01 +0400

    create onlineController and remove camera, geolocation controllers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) { 
 	$routeProvider.
-		when('/online', { templateUrl: '/template/online.html'}).
+		when('/online', { templateUrl: '/template/online.html', controller: 'onlineController'}).
 		when('/offline', { templateUrl: '/template/offline.html'}).
 		otherwise({ redirectTo: '/' });
 }]);
---
+++ js/controller/onlineController.js
@@ -0,0 +1,96 @@
+angular.module('app').
+controller('onlineController', ['$scope','geolocationService', 'cameraService', function($scope, geolocationService, cameraService) {
+	
+	$scope.controller = {
+	
+		serverUrl: 'http://localhost:8080',
+
+		getGeolocation: function() {		
+			var controller = this;
+	
+			geolocationService.getGeolocation({
+	
+				onSuccess: function(position) {
+					controller.sendGeolocation(position);	
+				},
+		
+				onError: function(error) {
+					controller.displayError(error);			
+				}
+		
+			});	
+		},
+
+		sendGeolocation: function(position) {
+			var controller = this;
+			var config = {
+				url: controller.serverUrl + "/geolocation",
+				data: position,
+				successMessage: "Your data sent to server"
+			};
+			controller.sendToServer(config);
+		},
+				
+		getPicture: function() {
+			var controller = this;
+
+			cameraService.getPicture({
+
+				onSuccess: function(picture) {
+					controller.sendPicture(picture);
+				},
+	
+				onError: function(error) {
+					controller.displayError(error);
+				}
+	
+			});
+		},
+
+		sendPicture: function(picture) {
+			var controller = this;
+			var config = {
+				url: controller.serverUrl + "/geolocation",
+				data: picture,
+				successMessage: "Your picture saved on server successly"
+			};
+			controller.sendToServer(config);
+		},
+		
+		sendToServer: function(config) {
+			var controller = this;
+	
+			$.ajax({
+				type: "POST",
+				url: config.url,
+		
+				data: {
+					data: JSON.stringify(config.data)
+				},
+		
+				success: function() {
+					controller.displaySuccess(config.successMessage);
+				},
+
+				error: function(error) {
+					controller.displayError(error);
+				}
+			  
+			});		     
+		},
+
+		displayError: function(error) {
+			if (error.code != undefined) {
+				alert('code: ' + error.code + ' ' + error.message);
+			}
+			else{
+				alert('code: ' + error.status + ' ' + error.statusText);
+			}
+		},
+
+		displaySuccess: function(message) {
+			alert(message);
+		}
+	};
+	
+}]);
